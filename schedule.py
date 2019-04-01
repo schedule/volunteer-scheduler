311b17c760a94bd3d990169f45b89069729f4a82
Imre Szakál <imre.szakal@gmail.com>
2019-04-01 12:16:34 +0200

    Added importing data from csv file

--- schedule.py
+++ schedule.py
@@ -1,62 +1,32 @@
 # https://github.com/imreszakal/helpline_scheduler
 
-input_list = []
-def input(id, type, days_available, workload, max_weekend_days,
-        welcomes_observer, separate_w, alone, cannot_alone, not_with):
-    input_list.append([id, type, days_available, workload, max_weekend_days,
-        welcomes_observer, separate_w, alone, cannot_alone, not_with])
-
-# /////////////////////// BEGINNING OF INPUT /////////////////////////////
-number_of_volunteers = 17
-days_in_month = 30 # April 2019
-first_monday = 1 # 1st of actual month is 1
-distance = 2 # Distance between workdays per person
-
-# input(
-# 1: Volunteer ID number,
-# 2: type, ('P':only phone  'C':only chat  'CP':chat and phone  'O':observer)
-# 3: [days available],
-# 4: maximum total workload,
-# 5: max weekend days,
-# 6: welcomes observer,
-# 7: wants shifts to be on separate weekends,
-# 8: wants to work alone,
-# 9: cannot work alone),
-# 10: does not want to work with
-#
-input(1,'P',[2,4,9,11,13,16,25,30],2,2,True,False,False,False,[])
-input(2,'CP',[4,6,8,9,11,12,14,15,18,19,20,21,22,23,25,26,28,29,30],4,2,False,False,False,False,[])
-input(3,'CP',[8,15,20,22,23,25,29],2,2,True,False,False,False,[])
-input(4,'P',[19,27],2,2,True,False,False,False,[])
-input(5,'P',[25],1,1,True,False,False,False,[])
-input(6,'P',[8,15],2,2,True,False,False,True,[])
-input(7,'P',[5,18,19,26],2,2,True,False,False,False,[])
-input(8,'CP',[3,14],2,2,True,False,False,False,[])
-input(9,'P',[1,3,4,6,7,10,11,13,17,18,21,24,25,27,28],3,1,False,False,False,False,[])
-input(10,'P',[5,16,19,26,29,30],2,2,False,False,False,False,[])
-input(11,'CP',[1,2,3,4,5,8,9,10,11,12,15],2,2,False,False,False,False,[8])
-input(12,'P',[4,11,25],2,2,False,False,False,False,[])
-input(13,'CP',[5,6,7,15,19,20,21,22,23,24],3,3,False,False,False,False,[])
-input(14,'C',[10,22,24,29],3,3,False,False,False,False,[])
-input(15,'P',[8,9,10,11,15,17,19,30],2,2,False,False,False,False,[])
-input(16,'P',[1,2,3,4,5,24,25,26],2,2,False,False,False,False,[])
-# Observer
-input(17,'O',[4,6,8,9,11,12,14,15,18,19,20,21,22,23,25,26,28,29,30],4,2,False,False,False,False,[])
-
-
-# ///////////////////////// END OF INPUT /////////////////////////////////
-
 from ortools.sat.python import cp_model
+import csv
+import calendar
 
 def main():
-    # Creates the model.
-    model = cp_model.CpModel()
+    # Get data from csv file
+    f = []
+    with open("schedule.csv") as csvfile:
+         reader = csv.reader(csvfile, delimiter=',')
+         for row in reader:
+             f.append(row)
+    schedule_year = int(f[0][0])
+    schedule_month = int(f[1][0])
+    month_name = calendar.month_name[schedule_month]
+    data_first = 3
+    data_last = len(f) - 1
 
-    # Volunteers ID list [1,2,3,...]
-    volunteers = [i for i in range(1, number_of_volunteers + 1)]
+    number_of_volunteers = len(f) - 3
+    # Volunteers ID list [0,1,2,3,...]
+    volunteers = [i for i in range(number_of_volunteers)]
 
+    # Days in month; firstday 0 if monday
+    firstday, days_in_month = calendar.monthrange(schedule_year, schedule_month)
     # Days' list [1,2,3,...]
     list_of_days = [i for i in range(1, days_in_month + 1)]
+    # First Monday of the month
+    first_monday = firstday + 1
 
     # Chat days' list (Mondays and Wednesdays)
     chat_days = []
@@ -101,6 +71,9 @@ def main():
         week_index += 1
         m += 7
 
+    # Distance between workdays per person
+    distance = 2
+
     # Phone: shift 0, Chat: shift 1, Observation: shift 2
     shifts = [0,1,2]
 
@@ -114,6 +87,9 @@ def main():
     # List of volunteer who does not want to work with someone
     not_with_them = []
 
+    # Creates the model.
+    model = cp_model.CpModel()
+
     # Creates shift variables.
     # schedule[(v, d, s)]: volunteer 'v' works shift 's' on day 'd'.
     schedule = {}
@@ -122,101 +98,130 @@ def main():
             for s in shifts:
                 i = 'shift_{:_>2}.{:_>2}.{}'.format(v, d, s)
                 schedule[(v, d, s)] = model.NewBoolVar(i)
+    all_days_available = []
+    all_workload = []
+    all_cannot_alone = []
+    all_not_with = []
 
     # Sets up class to processes input data
-    class Vol:
-        def __init__(self, id, type, days_available, workload,
-            max_weekend_days, welcomes_observer, separate_w, alone,
-            cannot_alone, not_with):
-            # Volunteers doing only chat
-            if type == 'C':
-                for d in list_of_days:
-                    for s in [0,2]:
-                        model.Add(schedule[(id, d, s)] == 0)
-                    if d in days_available:
-                        model.Add(schedule[(id, d, 1)] <= 1)
-                    else:
-                        model.Add(schedule[(id, d, 1)] == 0)
+    def use_volunteer_data(id, type, days_available, workload,
+        max_weekend_days, welcomes_observer, separate_w, alone,
+        cannot_alone, not_with):
 
-            # Volunteers doing chat and phone
-            if type == 'CP':
-                for d in list_of_days:
-                    model.Add(schedule[(id, d, 2)] == 0)
-                    if d in days_available:
-                        for s in [0,1]:
-                            model.Add(schedule[(id, d, s)] <= 1)
-                    else:
-                        for s in [0,1]:
-                            model.Add(schedule[(id, d, s)] == 0)
+        all_days_available.append(days_available)
+        all_workload.append(workload)
 
-            # Volunteers doing only phone
-            if type == 'P':
-                for d in list_of_days:
-                    for s in [1,2]:
-                        model.Add(schedule[(id, d, s)] == 0)
-                    if d in days_available:
-                        model.Add(schedule[(id, d, 0)] <= 1)
-                    else:
-                        model.Add(schedule[(id, d, 0)] == 0)
+        # Volunteers doing only chat
+        if type == 'C':
+            for d in list_of_days:
+                for s in [0,2]:
+                    model.Add(schedule[(id, d, s)] == 0)
+                if d in days_available:
+                    model.Add(schedule[(id, d, 1)] <= 1)
+                else:
+                    model.Add(schedule[(id, d, 1)] == 0)
 
-            # Volunteers doing observation
-            if type == 'O':
-                observers.append(id)
-                for d in list_of_days:
+        # Volunteers doing chat and phone
+        if type == 'CP':
+            for d in list_of_days:
+                model.Add(schedule[(id, d, 2)] == 0)
+                if d in days_available:
+                    for s in [0,1]:
+                        model.Add(schedule[(id, d, s)] <= 1)
+                else:
                     for s in [0,1]:
                         model.Add(schedule[(id, d, s)] == 0)
-                    if d in days_available:
-                        model.Add(schedule[(id, d, 2)] <= 1)
-                    else:
-                        model.Add(schedule[(id, d, 2)] == 0)
 
-            # List observers and volunteers welcoming observers
-            if type in ['C', 'CP', 'P'] and welcomes_observer:
-                welcomers.append(id)
+        # Volunteers doing only phone
+        if type == 'P':
+            for d in list_of_days:
+                for s in [1,2]:
+                    model.Add(schedule[(id, d, s)] == 0)
+                if d in days_available:
+                    model.Add(schedule[(id, d, 0)] <= 1)
+                else:
+                    model.Add(schedule[(id, d, 0)] == 0)
 
-            # Total workload
-            model.Add(sum(schedule[(id, d, s)]
-                for d in days_available for s in shifts) <= workload)
+        # Volunteers doing observation
+        if type == 'O':
+            observers.append(id)
+            for d in list_of_days:
+                for s in [0,1]:
+                    model.Add(schedule[(id, d, s)] == 0)
+                if d in days_available:
+                    model.Add(schedule[(id, d, 2)] <= 1)
+                else:
+                    model.Add(schedule[(id, d, 2)] == 0)
 
-            # Max weekend days
-            max_weekend_days
-            days = [d for d in weekend_days if d in days_available]
-            model.Add(sum(schedule[(id, d, s)]
-                for d in days for s in shifts) <= max_weekend_days)
+        # List observers and volunteers welcoming observers
+        if type in ['C', 'CP', 'P'] and welcomes_observer:
+            welcomers.append(id)
 
-            # Wants shifts to be on different weeks
-            if separate_w:
-                for week in weeks:
-                    model.Add(sum(schedule[(id, d, s)]
-                        for s in shifts for d in week
-                        if d in days_available) <= 1)
+        # Total workload
+        model.Add(sum(schedule[(id, d, s)]
+            for d in days_available for s in shifts) <= workload)
 
-            # Wants to work alone
-            if alone:
-                days = [d for d in days_available]
-                others = [i for i in volunteers if i != id]
-                for d in days:
-                    model.Add(sum(schedule[(v, d, s)] for v in others
-                        for s in shifts) + schedule[(id, d, s)] <= 1)
+        # Max weekend days
+        max_weekend_days
+        days = [d for d in weekend_days if d in days_available]
+        model.Add(sum(schedule[(id, d, s)]
+            for d in days for s in shifts) <= max_weekend_days)
 
-            # Cannot work alone
-            if cannot_alone:
-                cannot_work_alone.append(id)
+        # Wants shifts to be on different weeks
+        if separate_w:
+            for week in weeks:
+                model.Add(sum(schedule[(id, d, s)]
+                    for s in shifts for d in week
+                    if d in days_available) <= 1)
 
-            # Does not want to work with XY
-            if not_with:
-                not_with_them.append([id, not_with])
+        # Wants to work alone
+        if alone:
+            days = [d for d in days_available]
+            others = [i for i in volunteers if i != id]
+            for d in days:
+                model.Add(sum(schedule[(v, d, s)] for v in others
+                    for s in shifts) + schedule[(id, d, s)] <= 1)
+
+        # Cannot work alone
+        if cannot_alone:
+            cannot_work_alone.append(id)
+
+        # Does not want to work with XY
+        if not_with:
+            not_with_them.append([id, not_with])
 
 
+    volunteer_dic = {}
+    volunteer_dic_r = {}
+    i = data_first
+    while i <= data_last:
+        volunteer_dic[i-3] = f[i][0]
+        volunteer_dic_r[f[i][0]] = i-3
+        i += 1
 
-    # Puts input data into process
-    for i in input_list:
-        Vol(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9])
+    # 5,'P',[25,5,6,8],7,6,1,0,0,0,[])
+    i = data_first
+    while i <= data_last:
+        id = i-3
+        type = f[i][1]
+        days_available = [int(x) for x in f[i][2].split(',')
+            if x.strip().isdigit()]
+        workload = int(f[i][3])
+        max_weekend_days = int(int(f[i][4]))
+        welcomes_observer = bool(int(f[i][5]))
+        separate_w = bool(int(f[i][6]))
+        alone = bool(int(f[i][7]))
+        cannot_alone = bool(int(f[i][8]))
+        not_with = [volunteer_dic_r[name] for name in f[i][9].split(',')
+            if name]
+        use_volunteer_data(id, type, days_available, max_weekend_days, workload,
+            welcomes_observer, separate_w, alone, cannot_alone, not_with)
+        i += 1
 
     # Exactly one volunteer per shift.
     for d in list_of_days:
         # Phone shifts every day
-        model.Add(sum(schedule[(v, d, 0)] for v in volunteers) == 1)
+        model.Add(sum(schedule[(v, d, 0)] for v in volunteers) <= 1)
         # Chat shifts on chat days
         if d in chat_days:
             model.Add(sum(schedule[(v, d, 1)] for v in volunteers) <= 1)
@@ -249,8 +254,8 @@ def main():
                             schedule[(v, d, s)] == False)
 
     # Cannot work alone
-    for id in cannot_work_alone:
-        days = [d for d in input_list[id-1][2]]
+    for id in all_cannot_alone:
+        days = [d for d in all_days_available[id]]
         others = [i for i in volunteers if i != id
             and i not in cannot_work_alone]
         other_not_alones = [i for i in cannot_work_alone if i != id]
@@ -269,6 +274,7 @@ def main():
     # Does not want to work with XY
     for not_want in not_with_them:
         them = not_want[1]
+        days = [d for d in all_days_available[not_want[0]]]
         for d in days:
             model.Add(sum(schedule[(not_want[0], d, s)] for s in shifts)
                 and sum(schedule[(v, d, s)]
@@ -286,10 +292,14 @@ def main():
     solver.Solve(model)
 
     # Prints out workload for each volunteer
+    # print()
     print()
-    print('2019 April   -   Distance: ' + str(distance))
+    # calendar.setfirstweekday(calendar.MONDAY)
+    # print(calendar.month(schedule_year, schedule_month))
+    print(str(schedule_year) + ' ' + month_name + ' schedule:')
     print()
     for v in volunteers:
+        has = False
         for d in list_of_days:
             for s in shifts:
                 if solver.Value(schedule[(v, d, s)]) == 1:
@@ -299,34 +309,41 @@ def main():
                         t = 'chat'
                     elif s == 2:
                         t = 'observer'
-                    print('Volunteer {}: {}. {}'.format(v,d,t))
+                    if has:
+                        print(' ' * 12 + '{:>2}. {}'.format(d,t))
+                    else:
+                        print('{:>10}: '.format(volunteer_dic[v]) + '{:>2}. {}'.format(d,t))
+                    has = True
+        if has:
+            print()
     print()
 
     # Who works less than willing?
-    print('Remaining capacities: ', end='')
-    free_capacity = False
-    for v in volunteers:
-        works = 0
-        for d in list_of_days:
-            for s in shifts:
-                if solver.Value(schedule[(v, d, s)]) == 1:
-                    works += 1
-        more = input_list[v-1][3] - works
-        if more > 0:
-            free_capacity = True
-            more = input_list[v-1][3] - works
-            print('Free capacity of volunteer {}: {} day'\
-                .format(v, more), end='')
-            if more > 1:
-                print('s.')
-            else:
-                print('.')
-    if not free_capacity:
-        print('None')
-    print()
+    # print('Remaining capacities: ', end='')
+    # free_capacity = False
+    # for v in volunteers:
+    #     works = 0
+    #     for d in list_of_days:
+    #         for s in shifts:
+    #             if solver.Value(schedule[(v, d, s)]) == 1:
+    #                 works += 1
+    #     more = all_workload[v] - works
+    #     if more > 0:
+    #         free_capacity = True
+    #         more = input_list[v][3] - works
+    #         print('Free capacity of', volunteer_dic[v] + ': {} day'\
+    #             .format(v, more), end='')
+    #         if more > 1:
+    #             print('s.')
+    #         else:
+    #             print('.')
+    # if not free_capacity:
+    #     print('None')
+    # print()
 
-    print('Day|  P  C  O')
-    print('___|___________')
+    print(str(schedule_year) + ' ' + month_name)
+    print('Day|      Phone       Chat    Observer')
+    print('___|______________________________________')
     for d in list_of_days:
         phone, chat, observer = False, False, False
         print('{:>2}.| '.format(d), end='')
@@ -337,9 +354,9 @@ def main():
                 phone = True
                 break
         if phone:
-            print('{:>2} '.format(v), end='')
+            print('{:>10} '.format(volunteer_dic[v]), end='')
         else:
-            print(' _ ', end='')
+            print('      _    ', end='')
 
         # Chat
         for v in volunteers:
@@ -347,11 +364,11 @@ def main():
                 chat = True
                 break
         if chat:
-            print('{:>2} '.format(v), end='')
+            print('{:>10} '.format(volunteer_dic[v]), end='')
         elif d in chat_days:
-            print(' _ ', end='')
+            print('      _    ', end='')
         else:
-            print('   ', end='')
+            print('           ', end='')
 
         # Observer
         for v in volunteers:
@@ -359,11 +376,74 @@ def main():
                 observer = True
                 break
         if observer:
-            print('{:>2} '.format(v), end='')
+            print('{:>10} '.format(volunteer_dic[v]), end='')
         print()
     print()
     print()
     print()
 
+    print(str(schedule_year) + ' ' + month_name)
+    print('____________________________________________________________________________________________________________')
+    first_shift = ''
+    for i in range(firstday):
+        first_shift = ' ' * 14 * i
+    for week in weeks:
+        line_0 = ''
+        line_1 = ''
+        line_2 = ''
+        line_3 = ''
+        if first_shift:
+            line_0 += first_shift
+            line_1 += first_shift
+            line_2 += first_shift
+            line_3 += first_shift
+            first_shift = 0
+
+        for d in week:
+            phone, chat, observer = False, False, False
+            line_0 += '{:<2}'.format(d) + ' ' * 14
+
+            # Phone
+            for v in volunteers:
+                if solver.Value(schedule[(v, d, 0)]) == 1:
+                    phone = True
+                    break
+            if phone:
+                line_1 += 'P: {:<10}'.format(volunteer_dic[v]) + ' ' * 3
+            else:
+                line_1 += 'P: -' + ' ' * 12
+
+            # Chat
+            for v in volunteers:
+                if solver.Value(schedule[(v, d, 1)]) == 1:
+                    chat = True
+                    break
+            if chat:
+                line_2 += 'C: {:<10}'.format(volunteer_dic[v]) + ' ' * 3
+            elif d in chat_days:
+                line_2 += 'C: -' + ' ' * 12
+            else:
+                line_2 += ' ' * 16
+
+            # Observer
+            for v in volunteers:
+                if solver.Value(schedule[(v, d, 2)]) == 1:
+                    observer = True
+                    break
+            if observer:
+                line_3 += 'O: {:<10}'.format(volunteer_dic[v]) + ' ' * 3
+            else:
+                line_3 += ' ' * 16
+        print(line_0)
+        print(line_1)
+        print(line_2)
+        print(line_3)
+        print('____________________________________________________________________________________________________________')
+        # print()
+        # print()
+        # print()
+    print()
+    print()
+    print()
 if __name__ == '__main__':
     main()

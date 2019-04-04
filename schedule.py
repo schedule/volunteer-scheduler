12e2dceb431758479a41638d7d227e733a6e9d4c
Imre Szakál <imre.szakal@gmail.com>
2019-04-04 19:27:35 +0200

    Exception handling for command line argument

--- schedule.py
+++ schedule.py
@@ -2,16 +2,28 @@
 
 import sys
 
-language = sys.argv[1]
-
-if language == 'EN':
-    from language_EN import *
-elif language == 'HU':
-    from language_HU import *
-elif language ==     'CN':
-    from language_CN import *
-else:
-    sys.exit('Language setting is incorrect.')
+try:
+    language = sys.argv[1]
+    if language == 'EN':
+        from language_EN import *
+    elif language == 'HU':
+        from language_HU import *
+    elif language ==     'CN':
+        from language_CN import *
+    else:
+        print()
+        print('Error: Wrong language code.')
+        print('Choose between EN/HU/CN.')
+        print()
+        sys.exit()
+except IndexError:
+    print()
+    print('Usage:')
+    print('1. Choose language: EN/HU/CN.')
+    print('2. Export corresponding data_XX.csv file into a spreadsheet, fill in your data, then export back into this file.')
+    print('3. Run command: python3 schedule.py XX')
+    print()
+    sys.exit()
 
 from ortools.sat.python import cp_model
 import csv
